4087f019902edd413d77bf6fc7c7579b48522ab5
WestheLeaf <92562761+WestheLeaf@users.noreply.github.com>
2022-03-03 21:24:31 -0800

    h

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,19 @@ Route::get('/calendar', function () {
 Route::get('/board', function () {
     return view('board');
 });
+
+Route::get('events-feed/', function () {
+    return
+        '[
+            {
+              "title": "CSE4500 Class",
+              "start": "2022-02-23T17:30:00",
+              "end": "2022-02-23T18:45:00"
+            },
+            {
+              "title": "CSE4500 Class",
+              "start": "2022-02-28T17:30:00",
+              "end": "2022-02-28T18:45:00"
+            },   
+          ]';
+});
