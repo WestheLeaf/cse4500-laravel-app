495c050be0768ed87bc841c81b16958ac4b53b5e
WestheLeaf <92562761+WestheLeaf@users.noreply.github.com>
2022-03-11 18:32:18 -0800

    g

--- database/migrations/2022_03_12_022626_create_calendar_table.php
+++ database/migrations/2022_03_12_022626_create_calendar_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('calendar', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->date('start_at');
-            $table->date('end_at');
+            $table->datetime('start_at');
+            $table->datetime('end_at');
         });
     }
 
